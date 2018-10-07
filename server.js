37188befcbec7e7dbb28b73d1e8e97529a6d38b2
yichung <ql3895619@gmail.com>
2018-10-07 15:30:51 +0800

    Add feature: fetch raddar images

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const fs = require('fs')
 const https = require('https')
 const express = require('express')
 const config = require('./config')
+const dateAndTime = require('date-and-time')
+const util = require('util')
+const setTimeoutPromise = util.promisify(setTimeout)
 
 const linebot = require('./linebot')
-
 const app = express()
 
 const options = {
@@ -13,9 +15,9 @@ const options = {
   cert: fs.readFileSync('./ssl/certificate.crt')
 }
 
-https.createServer(options, app).listen(config.port,()=>{
+https.createServer(options, app).listen(config.port,
   console.log(`listen on port:  ${config.port}`)
-})
+)
 
 app.use(linebot.middleware)
 
@@ -29,4 +31,52 @@ app.post('/webhook', async (req, res) => {
 })
 
 const pushmsg = { type: 'text', text: 'this is a push message' }
-linebot.push(config.testId, pushmsg)
+//linebot.push(config.testId, pushmsg)
+
+const timeString = (time, minutes=0) => dateAndTime.format(dateAndTime.addMinutes(time, minutes), 'YYYYMMDDHHmm')
+
+const fetchImage = (time, verbose=false) => new Promise((resolve, reject) => { 
+  const fname = `CV1_3600_${timeString(time)}.png`
+  const path = `raddarImg/${fname}`
+  const url = `https://www.cwb.gov.tw/V7/observe/radar/Data/HD_Radar/${fname}`
+
+  if (fs.existsSync(path)) {
+    if (verbose) console.log(`${fname} exists, skip fetching`)
+    return resolve()
+  }
+
+  https.get(url, (response) => {
+    if (200 != response.statusCode) {
+      if (verbose) console.log(`fetch ${fname} failed`)
+      return reject()
+    }
+    response.pipe(fs.createWriteStream(path).on('close', () => {
+      return resolve()
+    }))
+  })
+})
+
+const fetchService = async (time) => {
+  try {
+    await fetchImage(time)
+    // analyze(time)
+    console.log(`${timeString(time)} succeed, next fetch in ${config.cwb.successTimeout / 60000} minute`)
+    await setTimeoutPromise(config.cwb.successTimeout)
+    time = dateAndTime.addMinutes(time, 10)
+    fetchService(time)
+  } catch(e) {
+    let now = new Date()
+    if (dateAndTime.subtract(now, time).toMinutes() > 30) {
+      let alert = { type: 'text', text: '<alert> FetchService failed. <alert>' }
+      linebot.push(config.testId, alert)
+      return
+    }
+    console.log(`${timeString(time)} failed, retry in ${config.cwb.failTimeout / 60000} minute`)
+    await setTimeoutPromise(config.cwb.failTimeout)
+    fetchService(time)
+  }
+}
+
+let time = new Date()
+time = dateAndTime.addMinutes(time, -parseInt(dateAndTime.format(time, 'mm')) % 10)
+fetchService(time)
